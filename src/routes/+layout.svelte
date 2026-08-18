fa757c9361336f7411b71cfd43522fa01eeb4de3
Daniel Irons <danielirons@protonmail.com>
2026-08-18 16:06:23 +0100

    add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
 	import PrintLayout from '$lib/components/print-layout.svelte';
